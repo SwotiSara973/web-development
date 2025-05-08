6007e5090b7f67d5e37341309e03c582c9f27411
SARA-collar <saraswotink@gmail.com>
2025-05-08 07:52:38 +0545

    Update assignment with latest changes

---
+++ js-firstclass/script.js
@@ -0,0 +1,43 @@
+/*console.log("this is external file ");
+let 
+var x = 'Rajesh hamal';
+console.log(x);
+
+x = 'Sharuk khan';
+console.log(x);*/
+
+let x = 15;
+let y = 8;
+
+let sum =  x + y ;
+let subtract = x - y ;
+console.log('sum', sum)
+console.log('subtract', subtract)
+ 
+
+if(x>y) {
+    console.log(x + 'is greater than '+ y);
+
+}else{
+    console.log(y + 'is grater than'+ x);
+}
+
+
+
+
+/*if (year = 2082);
+    {
+    console.log('this is current year');
+}
+else if (year>2082){
+    console.log('this is upcoming year');
+
+}
+else if( year<2082 ) {
+    console.log('this is passed year');
+}*/
+
+//conditions
+//<, >, = assignment operator,  == === data type equal xa or nai 
+// single line comment "//"
+console.log(9%5)
